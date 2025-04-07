68e8f5154a4f18b10425fa065cb800d734c2f77c
saheel <saheel.sarker@gmail.com>
2025-04-07 11:19:28 -0600

    yaml stuffs

--- obj/Debug/net8.0/ExpenseApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExpenseApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96820ec32cdbcfc37f00821cae103fb31e45ec87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a4ceaa3879b67e8d15ba68cb63e91d6cb15b6ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
